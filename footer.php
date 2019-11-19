566e7151f4a57e6d27d2b7cb2b19d6fb0c91867f
Thaylor Carvalho <thaylorcs@outlook.com>
2019-11-19 16:46:17 -0300

    Whatsapp Button

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 		<p>Thaylor Carvalho - 2019</p>
 		<a href="https://www.linkedin.com/in/thaylorcs/" target="_blank"><i class="fab fa-linkedin"></i></a>
 		<a href="https://github.com/thaylorcs" target="_blank"><i class="fab fa-github-square"></i></a>
+		<a href="https://api.whatsapp.com/send?phone=5511978014551"><i class="fab fa-whatsapp-square"></i></a>
 		<a href="mailto:contato@thaylorcarvalho.com"><i class="fas fa-envelope"></i></a>
 	</div>
 </div>
