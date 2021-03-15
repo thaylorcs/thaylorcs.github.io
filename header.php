f95cc165a67f12062fe9c42c1494a29453e457c7
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-15 17:24:14 -0300

    Remove local bootstrap, Layout fixes

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<title>Thaylor Carvalho</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="img/logoTC.png">
 	<link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   	<link rel="stylesheet" href="css/style.css">
   	<link rel="stylesheet" href="css/mobile.css">
 
@@ -16,7 +16,7 @@
 	
 	<div id="topo" class="container-fluid">
 		<header>
-			<div id="logo"><a href="index.php"><img src="img/logov2.png"></a></div>
+			<div id="logo"><a href="index.php"><img src="img/logov2.png" alt="Minha Logo"></a></div>
 			<nav class="menu float-right">
 				<ul>
 					<li><a href="index.php">home</a></li>
